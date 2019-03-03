9a700e44639f84a561af7d48c8f36e3789e639e1
sagitush <44786027+sagitush@users.noreply.github.com>
2019-03-03 20:58:29 +0200

    Add files via upload

--- 3.3.19/3.3.19/MyStack.cs
+++ 3.3.19/3.3.19/MyStack.cs
@@ -8,5 +8,43 @@ namespace _3._3._19
 {
     class MyStack
     {
+        private List<Mobile> mobiles1 = new List<Mobile>();
+        public MyStack()
+        {
+
+        }
+        public Mobile Pop ()
+        {
+            Mobile res = mobiles1[0];
+            mobiles1.RemoveAt(0);
+            return res;
+        }
+        public void push(Mobile mobile)
+        {
+            mobiles1.Add(mobile);
+        }
+        public int Size
+        {
+            get
+            {
+                return mobiles1.Count;
+            }
+        }
+        public  void Clear()
+        {
+            mobiles1.Clear();
+        }
+        public void Init(List<Mobile> list)
+        {
+            mobiles1 = list;
+        }
+        public void Sort ()
+        {
+            mobiles1.Sort();
+        }
+        public Mobile Peap()
+        {
+            return mobiles1[0];
+        }
     }
 }
